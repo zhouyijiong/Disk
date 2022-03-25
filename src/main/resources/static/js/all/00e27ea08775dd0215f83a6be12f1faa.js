f664a0310accbaa3e4ed617acb5d01fec19e0cb5
aress <3399690597@qq.com>
2022-03-25 10:35:48 +0800

    基本文件

---
+++ src/main/resources/static/js/all/00e27ea08775dd0215f83a6be12f1faa.js
@@ -0,0 +1,78 @@
+function checkStr(str,min,max){
+	var pattern=/^[0-9a-zA-Z]*$/;
+	return(str.length>=min&&str.length<=max)&&pattern.test(str)
+}
+
+function checkNum(str,min,max){
+	var pattern=/^[0-9]*$/;
+	return(str.length>=min&&str.length<=max)&&pattern.test(str)
+}
+
+function checkMail(str){
+	var reg=/^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+	return reg.test(str)
+}
+
+function fileSizeFormat(size){
+	var sizeInfo="";
+	if(size<1024){
+		sizeInfo=size+" B"
+	}else{
+		if(size<1048576){
+			size/=1024;
+			sizeInfo=size.toFixed(2)+" KB"
+		}else{
+			if(size<1073741824){
+				size/=1024;
+				size/=1024;
+				sizeInfo=size.toFixed(2)+" MB"
+			}else{
+				if(size<1099511627776){
+					size/=1024;
+					size/=1024;
+					size/=1024;
+					sizeInfo=size.toFixed(2)+" GB"
+				}else{
+					if(size<9999999999999){
+						size/=1024;
+						size/=1024;
+						size/=1024;
+						size/=1024;
+						sizeInfo=size.toFixed(2)+" TB"
+					}
+				}
+			}
+		}
+	}
+	return sizeInfo
+}
+
+function setCookie(name,value){
+	var Days = 7;
+	var exp = new Date();
+	exp.setTime(exp.getTime() + Days * 7 * 60 * 60 * 1000);
+	document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+
+function getCookie(name){
+	var arr,reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+	return (arr = document.cookie.match(reg)) ? unescape(arr[2]) : null;
+}
+
+function delCookie(name){
+	var exp = new Date();
+	exp.setTime(exp.getTime() - 1);
+	var cval = getCookie(name);
+    if(cval != null)document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+}
+
+function logout(){
+	$.ajax({
+		type:"POST",
+		url:"/file/logout",
+		dataType:"json",
+		success:function(data){
+			window.location.href="/"
+		}
+	})
+};
---
+++ src/main/resources/static/js/login/8e2ef0acc09e88f3d9224a7451157525.js
@@ -0,0 +1,62 @@
+window.onload=function(){
+	document.getElementById("usm").focus()
+}
+
+let login_state = 1;
+
+function updateState(obj){
+	let login = document.getElementById("login");
+	if(login_state === 1){
+		login_state = 0;
+		obj.innerHTML="login";
+		login.value="注册";
+	}else{
+		login_state = 1;
+		obj.innerHTML="registered";
+		login.value="登录";
+	}
+}
+function judgment(){
+	if(!navigator.cookieEnabled) {
+		alert("cookie 被禁用,请开启再使用本站");
+		return;
+	}
+	let usm = document.getElementById("usm");
+	if(!checkStr(usm.value,3,8)){
+		alert("用户名只能由3~10位字符或数字组成");
+		usm.value="";
+		usm.focus();
+		return
+	}
+	var pwd = document.getElementById("pwd");
+	if(!checkStr(pwd.value,6,16)){
+		alert("密码只能由6~16位字符或数字组成");
+		pwd.value="";
+		pwd.focus();
+		return
+	}
+	pwd.value = md5(pwd.value);
+	$.ajax({
+		type:"POST",
+		url:"/user/"+(login_state === 1 ? 'login' : 'registered'),
+		dataType:"json",
+		data:{"username":usm.value,"password":pwd.value},
+		success:function(data){
+			if(data.code === 500){
+				alert(data.msg);
+				usm.value="";
+				pwd.value="";
+				usm.focus();
+				return
+			}
+			localStorage.access = data.access;
+			window.location.href = "/";
+		}
+	})
+}
+document.getElementById("usm").addEventListener("keyup",function({keyCode}){
+	if(keyCode === 13) document.getElementById("pwd").focus();
+});
+document.getElementById("pwd").addEventListener("keyup",function({keyCode}){
+	if(keyCode === 13) judgment();
+});
